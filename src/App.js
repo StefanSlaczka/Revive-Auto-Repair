83f1e6a226b077b00150b4ca2bed640cf4153c84
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-07-05 12:04:44 -0400

    misson quot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Componnets/Navbar";
 import "./Style/App.css"
+import MissonQ from "./Componnets/MissonQ";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MissonQ/>
     </div>
   );
 }
---
+++ src/Componnets/MissonQ.jsx
@@ -0,0 +1,11 @@
+import "../Style/MissonQ.css"
+
+function MissonQ() {
+  return (
+    <div className="MissonQ">
+      Our mission is to revive the spirit of classic automobiles by meticulously restoring and enhancing each vehicle to its former glory. With a passion for craftsmanship and quality, we breathe new life into vintage cars, offering enthusiasts and collectors vehicles that blend timeless design with modern reliability. At Revive Auto Repair, we don't just sell cars; we preserve legacies.
+    </div>
+  );
+}
+
+export default MissonQ;
